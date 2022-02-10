f4a7929e4a777150c0a90af495ad28fa4fea1b1c
Jeffery Park <jp@Jefferys-MBP.cogeco.local>
2022-02-10 12:06:05 -0500

    Add data

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const users = ['Ian', 'Valentin', 'JP', 'Cassie', 'Stellaris'];
+
 module.exports = () => {
   router.get('/', (req, res) => {
-    res.json();
+    res.json(users);
   });
 
   return router;
