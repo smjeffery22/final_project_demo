a8acc51b750c741487a22e58ad2aff4a5a26fafa
Jeffery Park <jp@Jefferys-MBP.cogeco.local>
2022-02-10 12:01:02 -0500

    Refactor users route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+module.exports = () => {
+  router.get('/', (req, res) => {
+    res.json();
+  });
 
-module.exports = router;
+  return router;
+}
